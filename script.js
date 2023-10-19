e1415ba3dc6fe296823d2f90035e94454f8f5571
unknown <lukaezequiel15@gmail.com>
2023-10-19 11:46:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var swiperHeader = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,  // Corregido: slidesPerView en lugar de slidesPerview
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+    input.addEventListener('change', function() {
+        let id = input.value; // Cambié ariaValueMax a value para obtener el valor del input.
+        let thisSwiper = new Swiper('#swiper' + id, {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            }
+        });
+    });
+});
